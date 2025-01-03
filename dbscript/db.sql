761eeb1223dd35a58d8bfe999a9bf1563daedc2e
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2025-01-03 12:09:53 -0500

    Feat: 02-01-2025 - Script de base de datos.
    Incorporación del script al proyecto.

---
+++ dbscript/db.sql
@@ -0,0 +1,14 @@
+create database dbds20242;
+use dbds20242;
+
+create table tperson(
+idPerson char(36) not null,
+firstName varchar(70) not null,
+surName varchar(40) not null,
+dni char(8) not null,
+gender bool not null,
+birthDate date not null,
+createdAt datetime not null,
+updatedAt datetime not null,
+primary key(idPerson)
+) engine=innodb;
